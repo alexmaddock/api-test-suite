7756d908fc0ab14872fe86dc450ff7bb5f3a0d05
Alex Maddock <alexmaddock@Alexs-MBP-2.fritz.box>
2025-04-13 19:07:50 +1000

    init project and add mocha test suite

--- polling.js
+++ polling.js
@@ -8,8 +8,16 @@
  * 1. Base case - need an exit criteria to evaluate usually in the form of a conditional check: if input = 0; return
  * 2. Decrement to ensure we are going down in the number off attempts we make: attempts--
  * 3. Return statements on the values we return (cannot build without these otherwise it will fill run till it fills stack): return result / return functionName(params...)
- * 4. A means by which the functions nature is self referential: return recursivePoll(param1, param2NewState, param--)
+ * 4. A means by which the functions nature is self referential i.e. calling itself has all the necessary conditions to gracefully resolve: return recursivePoll(param1, param2NewState, param--)
  */
 const recursivePoll = () => {
-
+    // 1. Base Case
+    if(attemptsRemaining == 0) {
+        return $something; // 3
+    }
+    // 
+    else {
+        // 3. Return statement to continue executing
+        return recursivePoll() // 4. function self reference + 2. Decrement
+    }
 }
---
+++ test/numberAPITest.js
@@ -0,0 +1,20 @@
+import assert from 'assert';
+
+describe('Number API', ()=> {
+    
+    it('should poll basic math random function', ()=> {
+        
+        const generateRandomNumber = () => {
+            return Math.random();
+        };
+
+        
+
+    });
+
+
+    it('should poll actual number endpoint', ()=> {
+    
+    });
+
+});
