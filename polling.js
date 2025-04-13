a99044c59401394be4dae02b6d73ca356eae55db
Alex Maddock <alexmaddock@Alexs-MBP-2.fritz.box>
2025-04-13 21:30:35 +1000

    tried adding a condition to satisfy so I can select the array item as part of the exit condition of the loop, still not working

--- polling.js
+++ polling.js
@@ -10,20 +10,27 @@
  * 3. Return statements on the values we return (cannot build without these otherwise it will fill run till it fills stack): return result / return functionName(params...)
  * 4. A means by which the functions nature is self referential i.e. calling itself has all the necessary conditions to gracefully resolve: return recursivePoll(param1, param2NewState, param--)
  */
-const recursivePoll = (attemptsRemaining = 10, fn) => {
+const recursivePoll = async(attemptsRemaining = 10, fn, conditionToSatisfy) => {
     attemptsRemaining --; // 2. Decrement value to avoid forever loop.
-    const result = fn(); // call function from reference passed in.
+    const result = await fn(); // call function from reference passed in.
     console.log('My result is:', result);
     // 1. Base Case
     if(attemptsRemaining == 0) {
         return result; // 3. Return statement to have a final result.
+    }
+
+    console.log("Is this true:", conditionToSatisfy(result))
+
+    if(conditionToSatisfy(result)) { // add a truth statement to resolve - ideal for cb function use
+        console.log("Condition satisfied:", result)
+        return result;
     }
  
     else {
         console.log(`Return statement not resolved, result was ${result}, rerunning function.`);
         console.log(`Attempts remaining ${attemptsRemaining}`);
         // 3. Return statement to continue executing
-        return recursivePoll(attemptsRemaining, fn) // 4. function self reference.
+        return recursivePoll(attemptsRemaining, fn, conditionToSatisfy) // 4. function self reference.
     }
 }
 
--- test/numberAPITest.js
+++ test/numberAPITest.js
@@ -1,11 +1,10 @@
 import assert from 'assert';
 import getRandomNumber from '../numberAPI.js';
 import recursivePoll from '../polling.js';
-import { get } from 'http';
 
 describe('Number API', ()=> {
     
-    it('should poll basic math random function', ()=> {
+    it.skip('should poll basic math random function', ()=> {
         
         const generateRandomNumber = () => {
             return Math.random();
@@ -18,14 +17,14 @@ describe('Number API', ()=> {
 
 
     it('should poll actual number endpoint', async()=> {
-        // const randomNumber = await getRandomNumber();
+        const randomNumber = getRandomNumber;
         // console.log('Random Number is: ', randomNumber[0]);
 
-        recursivePoll(10, getRandomNumber);
+        await recursivePoll(10, randomNumber, (outcome) => {outcome < 50});
 
         // Might have to try something like const randomNumber = getRandomNumber(); await randomNumber[0]; to get index result
 
 
-    });
+    }).timeout(5000);
 
 });
