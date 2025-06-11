e703fcccffafbf7969f71be14c988d24aecfbfa7
Alex Maddock <alexmaddock@Alexs-MBP-2.fritz.box>
2025-06-11 20:42:37 +1000

    cleanup code

--- test/numberAPITest.js
+++ test/numberAPITest.js
@@ -1,4 +1,3 @@
-import assert from 'assert';
 import getRandomNumber from '../numberAPI.js';
 import recursivePoll from '../polling.js';
 
@@ -10,20 +9,16 @@ describe('Number API', ()=> {
             return Math.random();
         };
     
-        // const randomNumber = generateRandomNumber();
         recursivePoll(10, generateRandomNumber); // have to pass in generateRandomNumber as reference since internally poll calls the function
 
     });
 
 
     it('should poll actual number endpoint', async()=> {
+
         const randomNumber = getRandomNumber;
-        // console.log('Random Number is: ', randomNumber[0]);
 
         await recursivePoll(10, randomNumber, (outcome) => {return outcome[0] < 10});
-
-        // Might have to try something like const randomNumber = getRandomNumber(); await randomNumber[0]; to get index result
-
 
     }).timeout(5000);
 
