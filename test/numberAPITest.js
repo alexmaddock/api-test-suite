82d8f6b145ed90092a2c712607c1d465ff0094c3
Alex Maddock <alexmaddock@Alexs-MBP-2.fritz.box>
2025-06-13 19:05:03 +1000

    store readme update

--- test/numberAPITest.js
+++ test/numberAPITest.js
@@ -2,17 +2,6 @@ import getRandomNumber from '../numberAPI.js';
 import recursivePoll from '../polling.js';
 
 describe('Number API', ()=> {
-    
-    it.skip('should poll basic math random function', ()=> {
-        
-        const generateRandomNumber = () => {
-            return Math.random();
-        };
-    
-        recursivePoll(10, generateRandomNumber); // have to pass in generateRandomNumber as reference since internally poll calls the function
-
-    });
-
 
     it('should poll actual number endpoint', async()=> {
 
@@ -21,5 +10,15 @@ describe('Number API', ()=> {
         await recursivePoll(10, randomNumber, (outcome) => {return outcome[0] < 10});
 
     }).timeout(5000);
+
+    it.skip('should poll basic math random function', ()=> {
+
+        const generateRandomNumber = () => {
+            return Math.random();
+        };
+
+        recursivePoll(10, generateRandomNumber, (outcome) => {return outcome < 0.5}); // have to pass in generateRandomNumber as reference since internally poll calls the function
+
+    });
 
 });
